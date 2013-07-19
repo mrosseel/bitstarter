71d9bcbe3c638ca152c1a87fde88027f27dbe001
Mike Rosseel <mike@eon-consult.be>
2013-07-19 18:53:11 +0200

    HW3 changes

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,16 @@ var assertFileExists = function (infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function (htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function (buffer) {
+    return cheerio.load(buffer);
 };
 
 var loadChecks = function (checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function (htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function (buffer, checksfile) {
+    $ = cheerioHtmlFile(buffer);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for (var ii in checks) {
@@ -55,20 +56,38 @@ var checkHtmlFile = function (htmlfile, checksfile) {
     return out;
 };
 
+var handleFile = function(file) {
+    assertFileExists(file);
+    var checkJson = checkHtmlFile(fs.readFileSync(program.file), program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
+var handleUrl = function(url) {
+    rest.get(url).on('complete', function (result, response) {
+        var checkJson = checkHtmlFile(result, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+
+    });
+}
+
 var clone = function (fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var retrieveUrl = function(url) {
+
+}
+
 if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(handleFile), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Web address of your page', clone(handleUrl), "")
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
